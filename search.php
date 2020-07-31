bbfa84f0da6a42649fb768fd0b80a5e40ace0222
yottun <yotsuhashi.ken@gmail.com>
2020-08-01 02:57:11 +0900

    修正

--- search.php
+++ search.php
@@ -1,22 +1,12 @@
 <?php
-// DB接続の設定
-// DB名は`gsacf_x00_00`にする
-$dbn = 'mysql:dbname=gsacf_d06_35;charset=utf8;port=3306;host=localhost';
-$user = 'root';
-$pwd = '';
 
-try {
-    // ここでDB接続処理を実行する
-    $pdo = new PDO($dbn, $user, $pwd);
-} catch (PDOException $e) {
-    // DB接続に失敗した場合はここでエラーを出力し，以降の処理を中止する
-    echo json_encode(["db error" => "{$e->getMessage()}"]);
-    exit('dbError:' . $e->getMessage());
-}
-
+session_start();
+include('functions.php');
+check_session_id();
 // var_dump($_POST["category"]);
 // exit();
 
+$pdo = connect_to_db();
 
 // データ取得SQL作成
 $sql = 'SELECT * FROM kadai_recipe_table WHERE category ="' . $_POST["category"] . '"';
